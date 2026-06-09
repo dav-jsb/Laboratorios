e9ec80c8d08e8d18a7bae20a6ba387d691023a19
Davi José <davi.djsb@hotmail.com>
2026-06-09 16:52:43 -0300

    Add files via upload

--- project/rv32i_pipelined_base_project/src/pl_control.sv
+++ project/rv32i_pipelined_base_project/src/pl_control.sv
@@ -41,6 +41,7 @@ module pl_control (
     localparam LOAD   = 7'b0000011;
     localparam STORE  = 7'b0100011;
     localparam BRANCH = 7'b1100011;
+    localparam I_TYPE = 7'b0010011;
 
     always_comb begin
         ALUSrc   = 1'b0;
@@ -73,6 +74,12 @@ module pl_control (
             BRANCH: begin
                 Branch   = 1'b1;
                 ALUOp    = 2'b01;
+            end
+
+            I_TYPE: begin
+                ALUSrc   = 1'b1;
+                RegWrite = 1'b1;
+                ALUOp    = 2'b11;
             end
             default: ; // sinais permanecem em zero (seguro)
         endcase
